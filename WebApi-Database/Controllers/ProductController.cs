a77882b5867158f16f4503e48bc8dfecf8e42d37
wissenfsd@gmail.com <wissenfsd@gmail.com>
2024-04-07 09:52:02 +0300

    Add project files.

---
+++ WebApi-Database/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi_Database.DMO;
+
+namespace WebApi_Database.Controllers
+{
+	[Route("wissen/[controller]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+		AdventureWorks2019Context _context;
+        public ProductController(AdventureWorks2019Context context)
+        {
+            _context = context;	
+        }
+		[HttpGet]
+        public IActionResult Get()
+		{
+			var result =_context.Products.AsEnumerable<Product>();
+			return Ok(result);
+
+		}
+		[HttpGet]
+		public IActionResult GetById(int id)
+		{
+			var result = _context.Products.Where(s => s.ProductId == id).ToList();
+			return Ok(result);
+		}
+	}
+}
